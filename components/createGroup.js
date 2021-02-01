d83ce28bd997b82ce78e9b8f51523d495c80e927
dawoodmalhi <dawoodmalhi007@gmail.com>
2021-02-01 22:48:19 +0500

    Some Code edited

--- components/createGroup.js
+++ components/createGroup.js
@@ -100,9 +100,6 @@ function CreateGroup(props) {
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
             >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                
-              </span>
               Create Group
             </button>
           </div>
