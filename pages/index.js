88dbd82be826d3c0c665e3e4282a9e8f379e82f3
dawoodmalhi <dawoodmalhi007@gmail.com>
2021-03-06 01:24:04 +0500

    Completed the Front Page of the Website

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Image from 'next/image';
 
 export default function Home() {
   const router = useRouter();
@@ -15,7 +16,7 @@ export default function Home() {
     <div className="md:flex bg-white h-screen rounded-lg p-24 justify-center">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"></img>
+          <img src="/mainPageVector.png" alt="Versity App Vector" className="object-cover object-center rounded"></img>
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">What is Versity?
